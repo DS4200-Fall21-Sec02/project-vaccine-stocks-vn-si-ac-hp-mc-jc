ad5fa9688044844057f15200c60ecc564180eb0a
roecch <90482248+roecch@users.noreply.github.com>
2021-11-17 22:38:20 -0500

    static of both graphs work and hover for bar

--- js/visualization.js
+++ js/visualization.js
@@ -12,8 +12,6 @@
 
 // __________________________________________________________________________________________________
 
-
-
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 50, left: 60},
     width = 600 - margin.left - margin.right,
@@ -28,54 +26,6 @@ const svg1 = d3.select("#vis-svg-1")
 .append("g")
 .attr("transform", `translate(${margin.left},${margin.top})`);
 
-//Read the data
-d3.csv("https://raw.githubusercontent.com/DS4200-Fall21-Sec02/project-vaccine-stocks-vn-si-ac-hp-mc-jc/main/data/vaccine-stocks.csv").then(function (data) {
-
-
-  let dataAdjClose = data.filter(function(d){ return  (d.Measure == "Adj_Close")})
-  // group the data: I want to draw one line per group
-  const sumstat = d3.group(dataAdjClose, d => d.Name); // nest function allows to group the calculation per level of a factor
-
-  // Add X axis --> it is a date format
-  const x = d3.scaleLinear()
-  //.domain(d3.extent(data, function (d) { return d.Day; }))
-  .domain([0,500]) // this is hardcoded (for now)
-  .range([0, width]);
-  svg1.append("g")
-  .attr("transform", `translate(0, ${height})`)
-  .call(d3.axisBottom(x).ticks(5));
-
-  // Add Y axis
-  const y = d3.scaleLinear()
-  //.domain([0, d3.max(data, function (d) { return +d.Value; })])
-  .domain([0, d3.max(dataAdjClose, function (d) { return +d.Value; })])
-  .range([height, 0]);
-  svg1.append("g")
-  .call(d3.axisLeft(y));
-
-  // color palette
-  const colors = d3.scaleOrdinal()
-  .range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33'])
-  // Draw the line
-  svg1.selectAll(".line")
-  .data(sumstat)
-  .join("path")
-  .attr("fill", "none")
-  .attr("stroke", function (d) { return colors(d[0]) })
-  .attr("stroke-width", 1.5)
-  .attr("d", function (d) {
-    return d3.line()
-    .x(function (d) { return x(d.Day); })
-    .y(function (d) { return y(+d.Value); })
-    (d[1])
-  })
-
-})
-
-
-
-
-
 let svg2 = d3
 .select("#vis-svg-1")
 .append("svg")
@@ -89,10 +39,50 @@ var color = d3
 .domain(["Johnson & Johnson", "Novavax", "BioNTech", "Astrazeneca", "Inovio Pharmaceuticals", "Moderna"])
 .range(["#FF7F50", "#21908dff", "#fde725ff", "#b46fd7", "#FF0000", "#FF00FF"]);
 
+
 //Read the data
-d3.csv("https://raw.githubusercontent.com/DS4200-Fall21-Sec02/project-vaccine-stocks-vn-si-ac-hp-mc-jc/main/data/vaccine-stocks.csv").then(function(data) {
-  // line
-  // bar
+d3.csv("https://raw.githubusercontent.com/DS4200-Fall21-Sec02/project-vaccine-stocks-vn-si-ac-hp-mc-jc/main/data/vaccine-stocks.csv").then(function (data) {
+
+
+  let dataAdjClose = data.filter(function(d){ return  (d.Measure == "Adj_Close")})
+  // group the data: I want to draw one line per group
+  const sumstat = d3.group(dataAdjClose, d => d.Name); // nest function allows to group the calculation per level of a factor
+
+  // Add X axis --> it is a date format
+  const x1 = d3.scaleLinear()
+  //.domain(d3.extent(data, function (d) { return d.Day; }))
+  .domain([0,500]) // this is hardcoded (for now)
+  .range([0, width]);
+  svg1.append("g")
+  .attr("transform", `translate(0, ${height})`)
+  .call(d3.axisBottom(x1).ticks(5));
+
+  // Add Y axis
+  const y1 = d3.scaleLinear()
+  //.domain([0, d3.max(data, function (d) { return +d.Value; })])
+  .domain([0, d3.max(dataAdjClose, function (d) { return +d.Value; })])
+  .range([height, 0]);
+  svg1.append("g")
+  .call(d3.axisLeft(y1));
+
+  // color palette
+  const colors = d3.scaleOrdinal()
+  .range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33'])
+  // Draw the line
+  let lines = svg1.selectAll(".line")
+  .data(sumstat)
+  .join("path")
+  .attr("fill", "none")
+  .attr("stroke", function (d) { return colors(d[0]) })
+  .attr("stroke-width", 1.5)
+  .attr("d", function (d) {
+    return d3.line()
+    .x(function (d) { return x1(d.Day); })
+    .y(function (d) { return y1(+d.Value); })
+    (d[1])
+  })
+
+
   let dataVol = data.filter(function(d){ return  (d.Measure == "Volume")})
   let dataNest = d3.group(dataVol, d => d.Name)
   let dataMap = new Map()
@@ -122,7 +112,7 @@ d3.csv("https://raw.githubusercontent.com/DS4200-Fall21-Sec02/project-vaccine-st
 
   console.log(dataMap)
 
-  let x = d3.scaleBand()
+  let x2 = d3.scaleBand()
   .domain(dataMap.keys())
   .range([0, width])
   .padding([(0.05)])
@@ -130,7 +120,7 @@ d3.csv("https://raw.githubusercontent.com/DS4200-Fall21-Sec02/project-vaccine-st
   svg2
   .append("g")
   .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x))
+  .call(d3.axisBottom(x2))
   .call((g) =>
       g
       .append("text")
@@ -140,14 +130,14 @@ d3.csv("https://raw.githubusercontent.com/DS4200-Fall21-Sec02/project-vaccine-st
       .attr("text-anchor", "end")
   );
 
-  let y = d3
+  let y2 = d3
   .scaleLinear()
   .domain([0, d3.max(dataMap.values())])
   .range([height, 0]);
 
   svg2
   .append("g")
-  .call(d3.axisLeft(y))
+  .call(d3.axisLeft(y2))
   .call((g) =>
       g
       .append("text")
@@ -158,18 +148,107 @@ d3.csv("https://raw.githubusercontent.com/DS4200-Fall21-Sec02/project-vaccine-st
       .text()
   );
 
+  let tooltip = d3.select("#vis-svg-1")
+  .append("div")
+  .attr('class', 'tooltip')
+  .style("position", "absolute")
+  .style("z-index", "10")
+  .style("visibility", "hidden")
+
+  function hover(event, d) {
+    d3.select(this).attr("fill", "red")
+    console.log(d)
+    let coords = d3.pointer(event, svg2)
+    //Update Tooltip Position & value
+    tooltip
+    .style('top', coords[1] + 10 + 'px')
+    .style('left', coords[0] + 10 + 'px')
+    .text(d[0] + "\n" + parseInt(d[1]))
+    .style("visibility", "visible")
+  }
+
   let bars = svg2.append("g")
   .selectAll("rect")
   .data(dataMap)
   .join("rect")
-  .attr("transform", d => `translate(${x(d[0]) + x.bandwidth() / 4}, ${y(d[1])})`)
-  .attr("width", x.bandwidth() / 2)
-  .attr("height", d => height - y(d[1]))
+  .attr("transform", d => `translate(${x2(d[0]) + x2.bandwidth() / 4}, ${y2(d[1])})`)
+  .attr("width", x2.bandwidth() / 2)
+  .attr("height", d => height - y2(d[1]))
   .style("fill", function (d) {
     return color(d[0]);
   })
+  .on("mouseover", hover)
+  .on("mousemove", hover)
+  .on("mouseout", function () {
+    d3.select(this).attr("fill", function (d) {return color(d[0]);})
+    tooltip.style("visibility", "hidden");
+  });
 
+  //Brushing Code---------------------------------------------------------------------------------------------
 
+  // let brush1 = d3.brush();
+  // let brush2 = d3.brush();
+  //
+  // svg1
+  // .call(
+  //     brush1                // Add the brush feature using the d3.brush function
+  //     .extent([[0, 0], [width, height]]) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
+  //     .on("start brush", updateChart1) // Each time the brush selection changes, trigger the 'updateChart' function
+  // )
+  //
+  // svg2
+  // .call(
+  //     brush2                // Add the brush feature using the d3.brush function
+  //     .extent([[0, 0], [width, height]]) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
+  //     .on("start brush", updateChart2) // Each time the brush selection changes, trigger the 'updateChart' function
+  // )
+  //
+  // function updateChart1(brushEvent) {
+  //   if (brushEvent === null) {
+  //     return;
+  //   }
+  //   let company = [];
+  //   lines.data(sumstat)
+  //   .classed("selected", function (d) {
+  //     i => console.log(brushEvent, d[0], y1(d[1].get(i).Value))
+  //     if (isBrushed(brushEvent, x1(d[0]), i => y1(d[1].get(i).Value))) {
+  //       company.push(d[0]);
+  //       return true;
+  //     }
+  //   });
+  //
+  //   bars.classed("selected", function (d) {
+  //     return company.includes(d[0])
+  //   });
+  // }
+  //
+  // function updateChart2(brushEvent) {
+  //   if (brushEvent === null) {
+  //     return;
+  //   }
+  //   let company = [];
+  //   bars
+  //   .classed("selected", function (d) {
+  //     console.log(brushEvent, x2(d[0]), y2(d[1]))
+  //     if (isBrushed(brushEvent, x2(d[0]), y2(d[1]))) {
+  //       company.push(d[0]);
+  //       return true;
+  //     }
+  //   });
+  //
+  //   bars.classed("selected", function (d) {
+  //     return company.includes(d[0])
+  //   });
+  // }
+  //
+  // function isBrushed(brushEvent, cx, cy) {
+  //   if(brushEvent.selection !== null) {
+  //     let x0 = brushEvent.selection[0][0];
+  //     let x1 = brushEvent.selection[1][0];
+  //     let y0 = brushEvent.selection[0][1];
+  //     let y1 = brushEvent.selection[1][1];
+  //     return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1;    // This return TRUE or FALSE depending on if the points is in the selected area
+  //   }}
 })
 
 
